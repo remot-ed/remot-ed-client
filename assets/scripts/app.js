ec2fe4d4a6853aa5cd4fd927a6b3a1973278fc42
torystosse <tory.mae.stosse@gmail.com>
2020-04-15 15:19:53 -0400

    Created new question folder, moved question events
    - Created new question folder to house api + events + ui
    - Moved previously written events and api calls for questions from quiz
    to questions
    - Changed app.js to listen for question events

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,12 +3,12 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events')
-const quizEvents = require('./quiz/events')
+const questionEvents = require('./question/events')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   authEvents.addHandlers()
-  quizEvents.addHandlers()
+  questionEvents.addHandlers()
 })
