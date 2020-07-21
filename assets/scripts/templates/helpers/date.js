ebb1b3f5911236d497ad936c90e2e2492f4ab38d
torystosse <tory.mae.stosse@gmail.com>
2020-07-21 16:28:32 -0400

    Added feature: if startTime exists, show date/time
    - Added if/else block in show quiz
    - If start time already exists, show when quiz is, and in what classroom
    - If start time does not exist, show previously created scheduling bar
    - Added helper to convert startTime to JS Date object, in UTC string,
    for better user readability

---
+++ assets/scripts/templates/helpers/date.js
@@ -0,0 +1,11 @@
+// HELPER EXAMPLE
+// This helper would be used in a .handlebars file
+// with the syntax {{limit title 20}}
+
+'use strict'
+
+const date = (startTime) => {
+  return new Date(startTime)
+}
+
+module.exports = date
