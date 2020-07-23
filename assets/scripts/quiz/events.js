d2e31456f71a2dd70090fe089488e7d59a0356d8
Derek Murphy <derekmurphy1993@gmail.com>
2020-07-23 12:39:14 -0400

    Refactored and reorganized quiz/events.js
    Organized code into CRUD catagories
    Removed unused/outdated functions

--- assets/scripts/quiz/events.js
+++ assets/scripts/quiz/events.js
@@ -4,9 +4,13 @@ const questionApi = require('../question/api')
 const ui = require('./ui')
 const store = require('../store')
 const getFormFields = require('../../../lib/get-form-fields')
+
 // TODO:
 // create a function that checks if date is in the past or future
 // if in the future, throw an error message
+
+// Create
+
 const onShowCreateQuiz = event => {
   event.preventDefault()
   ui.onShowCreateQuizSuccess()
@@ -32,8 +36,20 @@ const onFinishQuiz = event => {
   api.finishQuiz()
     .then(api.getOneQuiz(store.quizData[0]._id)
       .then(ui.onFinishQuizSuccess))
-    // .then(ui.onFinishQuizSuccess)
-    // .then(onGetAllQuizzes(event))
+    .catch(console.error)
+}
+
+const onScheduleQuizToClassroom = () => {
+  event.preventDefault()
+  const classId = $(event.target).data('id')
+  const quizId = store.quizData.quiz._id
+  // console.log('class ', classId)
+  // console.log('quiz ', quizId)
+  api.addClassroomToQuiz(quizId, classId)
+    .then(console.log)
+    .catch(console.error)
+  api.addQuizToClassroom(quizId, classId)
+    .then(console.log)
     .catch(console.error)
 }
 
@@ -48,17 +64,33 @@ const onFinishQuiz = event => {
 //     .catch(console.error)
 // }
 
-const onFinishQuizEdit = event => {
-  event.preventDefault()
-  ui.onFinishQuizEditSuccess()
-  // event.preventDefault()
-  // console.log('quizData: ', store.quizData.quiz._id)
-  // console.log('store.questions: ', store.questions)
-  // api.updateQuestionsInQuiz()
-  //   .then(ui.onFinishQuizSuccess)
-  //   .then(onGetAllQuizzes(event))
-  //   .catch(console.error)
+// Read
+
+const onGetAllQuizzes = event => {
+//  event.preventDefault()
+  // const userId = store.user._id
+  api.getAllQuizzes()
+    .then(ui.onGetAllQuizzesSuccess)
+    .catch(console.error)
 }
+const onGetOneQuiz = event => {
+  event.preventDefault()
+  const quizId = $(event.target).data('id')
+  // console.log(quizId)
+  api.getOneQuiz(quizId)
+    .then(ui.onGetOneQuizSuccess)
+    .catch(console.error)
+}
+
+const onShowScheduleClassrooms = () => {
+  event.preventDefault()
+
+  api.getMyClassrooms()
+    .then(ui.onShowScheduleClassroomsSuccess)
+}
+
+// Updates
+
 const onShowEditQuiz = event => {
   event.preventDefault()
   // calls editQuiz ui
@@ -68,6 +100,7 @@ const onShowEditQuiz = event => {
     .then(ui.onGetOneQuizEditSuccess)
     .catch(console.error)
 }
+
 const onEditQuiz = event => {
   event.preventDefault()
   const quizId = store.quizData._id
@@ -80,6 +113,19 @@ const onEditQuiz = event => {
       .then(ui.onEditQuizSuccess))
     .catch(console.error)
 }
+
+const onFinishQuizEdit = event => {
+  event.preventDefault()
+  ui.onFinishQuizEditSuccess()
+  // event.preventDefault()
+  // console.log('quizData: ', store.quizData.quiz._id)
+  // console.log('store.questions: ', store.questions)
+  // api.updateQuestionsInQuiz()
+  //   .then(ui.onFinishQuizSuccess)
+  //   .then(onGetAllQuizzes(event))
+  //   .catch(console.error)
+}
+
 // onEditQuizSuccess will have to lead directly into editQuestion
 const onEditQuizSchedule = event => {
   event.preventDefault()
@@ -93,6 +139,9 @@ const onEditQuizSchedule = event => {
     .then(ui.onEditQuizScheduleSuccess)
     .catch(console.error)
 }
+
+// Destroy
+
 // get one quiz
 // if quiz owner is user id
 // loop through res.questions
@@ -125,21 +174,9 @@ const onDeleteQuiz = event => {
       }
     })
 }
-const onGetAllQuizzes = event => {
-//  event.preventDefault()
-  // const userId = store.user._id
-  api.getAllQuizzes()
-    .then(ui.onGetAllQuizzesSuccess)
-    .catch(console.error)
-}
-const onGetOneQuiz = event => {
-  event.preventDefault()
-  const quizId = $(event.target).data('id')
-  // console.log(quizId)
-  api.getOneQuiz(quizId)
-    .then(ui.onGetOneQuizSuccess)
-    .catch(console.error)
-}
+
+// Misc
+
 const onSingleQuizToTeacherDash = () => {
   event.preventDefault()
   ui.onSingleQuizToTeacherDashSuccess()
@@ -151,50 +188,32 @@ const onFinishQuizToTeacherDash = () => {
   ui.onFinishQuizToTeacherDashSuccess()
 }
 
-const onShowScheduleClassrooms = () => {
-  event.preventDefault()
-
-  api.getMyClassrooms()
-    .then(ui.onShowScheduleClassroomsSuccess)
-}
-
-const onScheduleQuizToClassroom = () => {
-  event.preventDefault()
-  const classId = $(event.target).data('id')
-  const quizId = store.quizData.quiz._id
-  // console.log('class ', classId)
-  // console.log('quiz ', quizId)
-  api.addClassroomToQuiz(quizId, classId)
-    .then(console.log)
-    .catch(console.error)
-  api.addQuizToClassroom(quizId, classId)
-    .then(console.log)
-    .catch(console.error)
-}
 const addHandlers = event => {
+  // CREATE
+  $('.create-quiz-button').on('click', onShowCreateQuiz)
   $('.create-quiz').on('submit', '#create-quiz', onCreateQuiz)
   $('.create-question').on('click', '.finish-quiz', onFinishQuiz)
-  $('.create-quiz-button').on('click', onShowCreateQuiz)
-  $('.create-quiz').on('click', '.back-to-td', onCreateQuizBack)
-  // need to edit once handlebars is integrated
-  $('.quiz-listing').on('click', '.edit-quiz-link', onShowEditQuiz)
-  // $('.edit-quiz').on('submit', onEditQuiz)
-  $('.quiz-listing').on('click', '.delete-quiz', onDeleteQuiz)
-  // need to edit once handlebars is integrated
-  // $('.get-quizzes').on('submit', onGetAllQuizzes)
-  // need to edit once handlebars is integrated
-  $('.quiz-listing').on('click', '.single-quiz-link', onGetOneQuiz)
-  $('.create-quiz-button').on('click', onShowCreateQuiz)
-  $('#single-quiz-listing').on('click', '.classroom-list-schedule', onShowScheduleClassrooms)
-  $('#finish-quiz-view').on('click', '.classroom-list-schedule', onShowScheduleClassrooms)
   $('#single-quiz-listing').on('click', '.classname-schedule', onScheduleQuizToClassroom)
-  $('#finish-quiz-view').on('click', '.classname-schedule', onScheduleQuizToClassroom)
-  $('#single-quiz-listing').on('click', '.quiz-to-teacher-dash', onSingleQuizToTeacherDash)
-  $('#finish-quiz-view').on('click', '.finish-quiz-to-teacher-dash', onFinishQuizToTeacherDash)
+
+  // READ
+  $('.quiz-listing').on('click', '.single-quiz-link', onGetOneQuiz)
+
+  $('#single-quiz-listing').on('click', '.classroom-list-schedule', onShowScheduleClassrooms)
+
+  // UPDATE
+  $('.quiz-listing').on('click', '.edit-quiz-link', onShowEditQuiz)
+  $('#single-quiz-listing').on('submit', '#edit-quiz', onEditQuiz)
   $('#single-quiz-listing').on('submit', '.schedule-quiz', onEditQuizSchedule)
   $('#finish-quiz-view').on('submit', '.schedule-quiz', onEditQuizSchedule)
-  $('#single-quiz-listing').on('submit', '#edit-quiz', onEditQuiz)
   $('#edit-single-question').on('click', '.finish-quiz-edits', onFinishQuizEdit)
+
+  // DESTROY
+  $('.quiz-listing').on('click', '.delete-quiz', onDeleteQuiz)
+
+  // MISC
+  $('.create-quiz').on('click', '.back-to-td', onCreateQuizBack)
+  $('#single-quiz-listing').on('click', '.quiz-to-teacher-dash', onSingleQuizToTeacherDash)
+  $('#finish-quiz-view').on('click', '.finish-quiz-to-teacher-dash', onFinishQuizToTeacherDash)
 }
 
 module.exports = {
